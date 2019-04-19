0b10463dd4151d166151dad192b7e0ad93fe6e29
GeorgHorhik <horhi20032003@gmail.com>
2019-04-19 18:20:08 +0300

    gulp was configure

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,58 @@
-// // const ==================================================
-var 
-    gulp            = require('gulp'),
-    postcss         = require('gulp-postcss'),
-    cssnext         = require('postcss-cssnext'),
-    cssImport       =require ('postcss-import'),
-    cssnano         =require('cssnano'),
-    fs              =require('fs'),
-    preCss          = require('precss'),
-    colorFuncion    =require('postcss-color-function'),
-    sugarss         = require('sugarss'),
-    sorting         = require('postcss-sorting'),
-    rename  = require('gulp-rename');
+const
+    gulp              = require('gulp'),
+    rename            = require('gulp-rename'),
+    postcss           = require('gulp-postcss'),
+    sugarss           = require('sugarss'),
+    sorting           = require('postcss-sorting'),
+    precss            =  require('precss'),
+    postcssShort      = require('postcss-short'),
+    cssnext           = require('cssnext'),
+    autoprefixer      =require('autoprefixer'),
+    browserSync       = require('browser-sync').create();
 
-//         var css = fs.readFileSync("app/css/main.css", "utf8")
-//     //post css =================================================
+var processors = [
+  precss(/* options */)
+];
+// Rename Task
+function sugar(){
+  return gulp.src('app/postcss/main.sss')
+  .pipe(postcss([sorting], { parser: sugarss }, [ autoprefixer() ]))
+  .pipe(cssnext({
+        compress: true
+    }))
+  .pipe(
+  postcss([
+    postcssShort(/* pluginOptions */)
+  ]))
+  .pipe(
+    postcss([
+      precss(/* options */)
+    ])
+  )
+  .pipe(rename('main.css'))
+  .pipe(gulp.dest('app/css'))
+  .pipe(browserSync.stream());
+};
 
-//         gulp.task('style', function() {
-//             // content
-    
-//             var
-//                 processors = [
 
-//                 cssImport,
-//                 cssnext({
-//                     'customProperties': true,
-//                     'colorFuncion': true,
-//                     'customSelectors': true,
-                    
-//                 }),
-//                 preCss({
+function browser_sync () {
+  // sddf...
+  browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+}
 
-//                 }),
-//                 cssnext({ browsers:[ "> 1%",
-//                 "last 2 versions",
-//                 "ie >= 11" ] })
-//             ]
 
-//         return gulp.src('app/postCSS/main.css')
-//             .pipe(postcss(processors,  sugar))
-//             .pipe(gulp.dest('app/css'));
-//         });
-// // precss ==================================================
 
-// gulp.task('sort', function () {
-//     return gulp.src('app/postCSS/*.sss')
-//         .pipe(postcss([sorting], { syntax: sugarss }))
-//         .pipe(gulp.dest('app/css'));
-// });
-// // watch ===================================================
-//         gulp.task('watch', function() {
-//         gulp.watch('**/*.css', gulp.parallel('style'));
+function watch(){
+gulp.watch('app/postcss/*.sss', gulp.parallel('sugar'));
+gulp.watch("*.html").on('change', browserSync.reload);
+}
+exports.watch           = watch;
+exports.browser_sync    = browser_sync;
+exports.sugar           = sugar;
+exports.precss          = precss;
 
-//     });
-
-// // gulp Default ============================================='
-//     gulp.task('default', gulp.parallel('style', 'watch'));
-
- 
-gulp.task('style', function () {
-    return gulp.src('app/postCSS/*.sss')
-        .pipe(postcss(plugins, { parser: sugarss }))
-        .pipe(rename({ extname: '.css' }))
-        .pipe(gulp.dest('app/css'));
-        postcss().process(css, { stringifier: sugarss }).then(function (result) {
-            result.content // Converted SugarSS content
-        });
-});
+// [sorting], { parser: sugarss },
