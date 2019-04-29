9dddba8916c7c9c94666d1c4b37119d6981fd416
horhi <horhi20032003@gmail.com>
2019-04-29 20:38:27 +0300

    PRE CLEAN

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,31 @@ const
 gulp        = require('gulp'),
 rename      = require('gulp-rename'),
 postcss     = require('gulp-postcss'),
-sugarss     = require('sugarss'),
 sorting     = require('postcss-sorting'),
 precss      =  require('precss'),
 postcssShort = require('postcss-short'),
+stylus      = require('gulp-stylus'),
 browserSync = require('browser-sync').create();
-// you need do add @import
 
-var processors = [
-  precss(/* options */)
-];
-var csshort = [
-  postcssShort()
-];
-// Rename Task
-function sugar(){
-  return gulp.src('app/postcss/main.sss')
-  .pipe(postcss([sorting], { parser: sugarss }, [postcssShort()]))
-  .pipe(
-    postcss([
-      precss(/* options */)
-    ])
-  )
-  .pipe(rename('main.css'))
-  .pipe(gulp.dest('app/css'))
-  .pipe(browserSync.stream());
+'use strict';
+function css(){
+  return gulp.src('./app/stylus/main.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./app/css'))
+    .pipe(browserSync.stream());
 };
 
 
+gulp.task('autoprefixer', function () {
+  var sourcemaps   = require('gulp-sourcemaps');
+  var autoprefixer = require('autoprefixer');
 
-
-
+  return gulp.src('./app/*.css')
+      .pipe(sourcemaps.init())
+      .pipe(postcss([ autoprefixer() ]))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('./app/css'));
+});
 
 function reload(){
   browserSync.init({
@@ -40,15 +34,13 @@ function reload(){
         baseDir: "./"
     }
     });
-    gulp.watch('app/postcss/*.sss', gulp.parallel('sugar'));
+    gulp.watch('app/stylus/*.styl', gulp.series('css', 'autoprefixer'));
     gulp.watch('./*.html').on('change', browserSync.reload);
 }
 
 
 exports.reload = reload;
-exports.sugar = sugar;
-exports.precss = precss;
-
+exports.css    = css;
 
 gulp.task('default', gulp.parallel(reload))
 // [sorting], { parser: sugarss },
